89041446f43b14f8c481bbd9ce7442c3f171b857
JP <1.4.gigz@gmail.com>
2019-04-29 17:02:50 -0500

    First commit - add initial project files

---
+++ src/test/java/csnam/ed/java/AppTest.java
@@ -0,0 +1,20 @@
+package csnam.ed.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
