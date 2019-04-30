9bf868b09d97d55f0f3980806c7ddd3c036fe3b0
JP <1.4.gigz@gmail.com>
2019-04-29 21:42:28 -0500

    Set up index controller

--- src/main/java/csnam/ed/java/controllers/IndexController.java
+++ src/main/java/csnam/ed/java/controllers/IndexController.java
@@ -1,10 +1,22 @@
 package csnam.ed.java.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @project crmjava
  * @user jp
  * @date 2019-04-29
+ * This is the controller for the Customer model
  */
-public class CustomerController {
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+  @RequestMapping("/")
+  public String index(Model model) {
+    return "index";
+  }
 
 }
